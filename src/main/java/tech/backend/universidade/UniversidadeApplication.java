f5238e920a3a5c29ce1def6ab854ae7f77e5be99
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-03-20 16:39:20 -0300

    Configuring MySQL in Docker

--- src/main/java/tech/backend/universidade/UniversidadeApplication.java
+++ src/main/java/tech/backend/universidade/UniversidadeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UniversidadeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UniversidadeApplication.class, args);
 	}
---
+++ src/main/java/tech/backend/universidade/entity/User.java
@@ -0,0 +1,4 @@
+package tech.backend.universidade.entity;
+
+public class User {
+}
