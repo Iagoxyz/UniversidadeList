245e4a9bcd713c0dc5dcea567cb6f379edf53069
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-03-20 13:45:39 -0300

    Project created

---
+++ src/test/java/tech/backend/universidade/UniversidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.backend.universidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
